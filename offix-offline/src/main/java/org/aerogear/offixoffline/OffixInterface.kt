591168d901e1223b56d1ac402eec33ecb6c44440
Lavanya Gaur <lavanyagaur22@gmail.com>
2019-07-23 17:17:41 +0530

    fix: codacy errors

--- offix-offline/src/main/java/org/aerogear/offixoffline/OffixInterface.kt
+++ offix-offline/src/main/java/org/aerogear/offixoffline/OffixInterface.kt
@@ -9,7 +9,6 @@ import com.apollographql.apollo.exception.ApolloException
  OffixInterface is used by the users in making a callback object which is passed to the Offix enqueue() function.
  */
 interface OffixInterface {
-
     /**
      * Called when the request is successfully executed and we get the response from the server.
      */
