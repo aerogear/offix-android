ff2b04694188150cb60491a5ad1c5fe033f52eb4
harshithdwivedi <harshit@pitech.app>
2019-08-05 19:46:46 +0530

    fix : codacy errors
    Signed-off-by: harshithdwivedi <harshit@pitech.app>

--- offix/src/main/java/org/aerogear/offix/interceptor/ConflictInterceptor.kt
+++ offix/src/main/java/org/aerogear/offix/interceptor/ConflictInterceptor.kt
@@ -4,13 +4,14 @@ import android.util.Log
 import okhttp3.Interceptor
 import okhttp3.Response
 import org.aerogear.offix.detectedConflict
+import org.aerogear.offix.responseWithConflicts
 import java.nio.charset.Charset
 
 /*
  ConflictInterceptor should be used by the user while making an instance of apollo client
  which will help in detecting conflicts.
  */
-class ConflictInterceptor  : Interceptor{
+class ConflictInterceptor : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
@@ -25,9 +26,9 @@ class ConflictInterceptor  : Interceptor{
         Log.d("OffixClass", " Interceptor ** : $responseBodyString")
 
         if (responseBodyString.contains("conflictInfo")) {
-            detectedConflict = responseBodyString
+            responseWithConflicts = responseBodyString
             Log.d("ConflictInterceptor", " ******")
-            Log.d("ConflictInterceptor **", detectedConflict)
+            Log.d("ConflictInterceptor **", responseWithConflicts)
         }
         return response
     }
