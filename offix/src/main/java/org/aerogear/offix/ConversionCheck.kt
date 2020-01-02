36573dab73a3d94fdd5a21133203d078db516e94
ParthPali <54657955+ParthPali@users.noreply.github.com>
2020-01-02 12:33:33 -0800

    add offix unit test

---
+++ offix/src/main/java/org/aerogear/offix/ConversionCheck.kt
@@ -0,0 +1,12 @@
+package org.aerogear.offix
+
+import com.apollographql.apollo.api.OperationName
+
+object ConversionCheck{
+    fun checkStringisJson(s: String): Boolean {
+        return s.startsWith("{")&&s.endsWith("}")
+    }
+    fun operationNameExists(n: OperationName): Boolean{
+        return !n.name().isBlank()
+    }
+}
--- offix/src/main/java/org/aerogear/offix/persistence/Converters.kt
+++ offix/src/main/java/org/aerogear/offix/persistence/Converters.kt
@@ -3,6 +3,8 @@ package org.aerogear.offix.persistence
 import android.arch.persistence.room.TypeConverter
 import com.apollographql.apollo.api.OperationName
 import org.json.JSONObject
+import org.aerogear.offix.ConversionCheck
+import java.lang.Exception
 
 /*
 Converters are used to serialise and de-serialise objects for storing and fetching from database.
@@ -16,11 +18,21 @@ class Converters {
 
     @TypeConverter
     fun OperationNameToString(name: OperationName): String {
-        return name.name()
+        if(ConversionCheck.operationNameExists(name)) {
+            return name.name()
+        } else {
+            throw Exception("EmptyOperationNameException")
+        }
     }
 
     @TypeConverter
     fun StringToJson(string: String): JSONObject {
+        if(ConversionCheck.checkStringisJson(string)) {
+            val jsonObject = JSONObject(string)
+            return jsonObject
+        } else {
+            return JSONObject("{}")
+        }
         val jsonObject = JSONObject(string)
         return jsonObject
     }
