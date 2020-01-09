c62d20f9ede10175e258228d6af7298c5ea0f381
sayam <sawaisayam@gmail.com>
2020-01-09 06:48:35 +0530

    Added Instrumentation Tests for Sample App

--- sample/src/androidTest/java/org/aerogear/graphqlandroid/ExampleInstrumentedTest.kt
+++ sample/src/androidTest/java/org/aerogear/graphqlandroid/ExampleInstrumentedTest.kt
@@ -1,5 +1,6 @@
 package org.aerogear.graphqlandroid
 
+import android.media.session.MediaSession
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
 
@@ -8,6 +9,20 @@ import org.junit.runner.RunWith
 
 import org.junit.Assert.*
 
+// Imports
+import android.util.Log
+import org.aerogear.graphqlandroid.*
+import org.aerogear.graphqlandroid.adapter.TaskAdapter
+import org.aerogear.graphqlandroid.model.UserOutput
+import org.aerogear.graphqlandroid.type.TaskInput
+import org.aerogear.graphqlandroid.type.UserInput
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloQueryWatcher
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import com.apollographql.apollo.fetcher.ApolloResponseFetchers
+import kotlinx.android.synthetic.main.item_row_tasks.view.*
+
 /**
  * Instrumented test, which will execute on an Android device.
  *
@@ -15,10 +30,141 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    val appContext = InstrumentationRegistry.getTargetContext()
+    private var apolloQueryWatcher: ApolloQueryWatcher<FindAllTasksQuery.Data>? = null
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.lavanya.graphqlandroid", appContext.packageName)
+        // Context of the app under tests
+        assertEquals("org.aerogear.graphqlandroid", appContext.packageName)
     }
+
+    @Test
+    fun findAllTasksQuery(){
+
+        FindAllTasksQuery.builder()?.build()?.let{
+            Utils.getApolloClient(this.appContext)?.query(it)
+                ?.responseFetcher(ApolloResponseFetchers.NETWORK_FIRST)
+                ?.enqueue(object : ApolloCall.Callback<FindAllTasksQuery.Data>(){
+
+                    override fun onFailure(e: ApolloException) {
+                        assertTrue(false)
+                    }
+
+                    override fun onResponse(response: Response<FindAllTasksQuery.Data>) {
+                        assertTrue(true)
+                    }
+                })
+        }
+    }
+
+    @Test
+    fun findAllUsersQuery(){
+
+        FindAllUsersQuery.builder()?.build()?.let{
+            Utils.getApolloClient(appContext)?.query(it)
+                ?.responseFetcher(ApolloResponseFetchers.NETWORK_FIRST)
+                ?.enqueue(object: ApolloCall.Callback<FindAllUsersQuery.Data>(){
+
+                    override fun onFailure(e: ApolloException) {
+                        assertTrue(false)
+                    }
+
+                    override fun onResponse(response: Response<FindAllUsersQuery.Data>) {
+                        assertTrue(true)
+                    }
+                })
+        }
+    }
+
+    @Test
+    fun createTask(){
+
+        val input = TaskInput.builder().title("Test Task 2").version(1).description("Test Description").status("test")
+            .build()
+        val mutation = CreateTaskMutation.builder().input(input).build()
+
+        Utils.getApolloClient(appContext)
+            ?.mutate(mutation)
+            ?.refetchQueries(apolloQueryWatcher?.operation()?.name())
+            ?.enqueue(object: ApolloCall.Callback<CreateTaskMutation.Data>(){
+
+                override fun onFailure(e: ApolloException) {
+                    assertTrue(false)
+                }
+
+                override fun onResponse(response: Response<CreateTaskMutation.Data>) {
+                    assertTrue(true)
+                }
+            })
+    }
+
+    @Test
+    fun updateTask(){
+
+        val input = TaskInput.builder().title("Test Task Changed").version(1).description("Test Desc").status("test").build()
+        val mutation = UpdateTaskMutation.builder().id("6").input(input).build()
+
+        Utils.getApolloClient(appContext)
+            ?.mutate(mutation)
+            ?.refetchQueries(apolloQueryWatcher?.operation()?.name())
+            ?.enqueue(object: ApolloCall.Callback<UpdateTaskMutation.Data>(){
+
+                override fun onFailure(e: ApolloException) {
+                    assertTrue(false)
+                }
+
+                override fun onResponse(response: Response<UpdateTaskMutation.Data>) {
+                    assertTrue(true)
+                }
+            })
+    }
+
+    @Test
+    fun createUser(){
+        val input = UserInput.builder().taskId("6").email("sample@example.com").firstName("ezio").lastName("auditore")
+            .title("User Test")
+            .creationmetadataId("6").build()
+
+        val mutation = CreateUserMutation.builder().input(input).build()
+
+        Utils.getApolloClient(appContext)
+            ?.mutate(mutation)
+            ?.refetchQueries(apolloQueryWatcher?.operation()?.name())
+            ?.enqueue(object: ApolloCall.Callback<CreateUserMutation.Data>(){
+
+                override fun onFailure(e: ApolloException) {
+                    assertTrue(false)
+                }
+
+                override fun onResponse(response: Response<CreateUserMutation.Data>) {
+                    assertTrue(true)
+                }
+            })
+    }
+
+    @Test
+    fun checkAndUpdateTask(){
+
+        val mutation = CheckAndUpdateTaskMutation.builder().version(1).id("6").title("Test Title returns")
+            .description("Test Description Returns")
+            .status("Still Testing")
+            .build()
+
+        Utils.getApolloClient(appContext)
+            ?.mutate(mutation)
+            ?.refetchQueries(apolloQueryWatcher?.operation()?.name())
+            ?.enqueue(object: ApolloCall.Callback<CheckAndUpdateTaskMutation.Data>(){
+
+                override fun onFailure(e: ApolloException) {
+                    assertTrue(false)
+                }
+
+                override fun onResponse(response: Response<CheckAndUpdateTaskMutation.Data>) {
+                    assertTrue(true)
+                }
+            })
+    }
+
 }
