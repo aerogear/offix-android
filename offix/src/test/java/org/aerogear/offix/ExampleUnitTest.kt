2520cf0695dd12bc10b50aea1c59a61964f896f5
sayam <sawaisayam@gmail.com>
2020-01-19 15:26:56 +0530

    Done

--- offix/src/test/java/org/aerogear/offix/ExampleUnitTest.kt
+++ offix/src/test/java/org/aerogear/offix/ExampleUnitTest.kt
@@ -7,8 +7,6 @@ import com.apollographql.apollo.api.OperationName
 import org.json.JSONObject
 
 import org.aerogear.offix.persistence.Converters
-
-import com.google.gson.JsonParser
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
@@ -16,27 +14,31 @@ import com.google.gson.JsonParser
  */
 class ExampleUnitTest {
 
+    // Dummy Test
     @Test
     fun addition_isCorrect() {
         Assert.assertEquals(4, 2 + 2)
     }
 
+    // Check String To Json
     @Test
     fun CheckStringToJson(){
-        val name = Converters().StringToJson("{\"name\":\"sayam\"}")
-        print(name.toString())
+        val testJson:JSONObject = Converters().StringToJson("{'name': 'testName'}")
+        Assert.assertEquals("testName",testJson["name"])
     }
 
+    // Check String to Operation Name
     @Test
     fun CheckStringToOperName(){
-        val testOperation = Converters().StringToOperName("Test")
+        val testOperation:OperationName = Converters().StringToOperName("Test")
         Assert.assertEquals("Test",testOperation.name())
     }
 
+    // Check Operation to String
     @Test
     fun CheckOperationToString(){
         val testOperation = OperationName { "Test Operation" }
-        val testString = Converters().OperationNameToString(testOperation)
+        val testString:String = Converters().OperationNameToString(testOperation)
 
         Assert.assertEquals("Test Operation",testString)
     }
